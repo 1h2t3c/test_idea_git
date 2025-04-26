5f31f3475115c5d0db14e70975b31ccbcf95a926
huangtianci <2801270233@qq.com>
2025-04-26 15:16:59 +0800

    aaa

--- src/com/coder/Test.java
+++ src/com/coder/Test.java
@@ -6,12 +6,6 @@ package com.coder;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("fix1");
-        System.out.println("fix1");
-        System.out.println("fix1");
-        System.out.println("fix1");
+
     }
 }
